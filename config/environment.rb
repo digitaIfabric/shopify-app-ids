a107adc68bcdea17deeca5dc0fea1a37ce080f5c
digitalfabric92 <dwawryko@gmail.com>
2017-11-09 14:13:41 -0500

    Added keys

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
-API_KEY = 6b370eff671cb6c33937c66a23bf0bd7
-API_SECRET = 9152a86ae7ef9c721188697e8dac6afb
+# API_KEY = 6b370eff671cb6c33937c66a23bf0bd7
+# API_SECRET = 9152a86ae7ef9c721188697e8dac6afb
--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "6b370eff671cb6c33937c66a23bf0bd7" #ENV["API_KEY"]
-  config.secret = "9152a86ae7ef9c721188697e8dac6afb" #ENV["API_SECRET"]
+  config.api_key = ENV["API_KEY"]
+  config.secret = ENV["API_SECRET"]
   config.scope = "read_orders, read_products"
   # config.redirect_uri = "http://appinfifteen.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true

c8fa5070832fd60b6062f2c15b47302119fd8c04
digitalfabric92 <dwawryko@gmail.com>
2017-11-09 14:05:02 -0500

    Updated api key

--- config/initializers/shopify_app.rb
+++ config/initializers/shopify_app.rb
@@ -1,7 +1,7 @@
 ShopifyApp.configure do |config|
   config.application_name = "My Shopify App"
-  config.api_key = "<api_key>"
-  config.secret = "<secret>"
+  config.api_key = ENV["API_KEY"]
+  config.secret = ENV["API_SECRET"]
   config.scope = "read_orders, read_products"
   # config.redirect_uri = "http://appinfifteen.herokuapp.com/auth/shopify/callback"
   config.embedded_app = true

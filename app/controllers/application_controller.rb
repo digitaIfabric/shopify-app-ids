5068d0862b722e624ca25016cc9ae52b04741012
digitalfabric92 <dwawryko@gmail.com>
2017-11-13 18:01:27 -0500

    Updated links

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,14 +3,3 @@ require 'csv'
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 end
-
-def index
-  @products = Product.all
-  respond_to do |format|
-    format.html
-    format.csv do
-      headers['Content-Disposition'] = "attachment; filename=\"product-list\""
-      headers['Content-Type'] ||= 'text/csv'
-    end
-  end
-end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,12 @@ class HomeController < ShopifyApp::AuthenticatedController
   def index
     @products = ShopifyAPI::Product.find(:all, params: { limit: 10 })
     @webhooks = ShopifyAPI::Webhook.find(:all)
+    respond_to do |format|
+      format.html
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"product-list\""
+        headers['Content-Type'] ||= 'text/csv'
+      end
+    end
   end
 end
